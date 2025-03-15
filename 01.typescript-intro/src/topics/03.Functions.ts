c45c78412eb824f788fdf37c4dc3e089c2767bda
DanielRodriguezZara <danielrodrizarate@gmail.com>
2025-03-15 15:46:48 -0600

    Functions

---
+++ 01.typescript-intro/src/topics/03.Functions.ts
@@ -0,0 +1,20 @@
+//La función sumarNum recibe dos parametros tipo number y devuelve un valor tipo number
+function sumarNum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+//Lambda functions(funciones de flecha)
+//Esta función puede recibir 3 argumentos, uno es obligatorio, otro es opcional y si el ultimo parametro no se le da un valor sera 2
+const sumarElementos = (
+  num1: number,
+  num2?: number,
+  num3: number = 2
+): string => {
+  return `La suma de ${num1} y ${num3} es ${num1 + num3}`;
+};
+
+const suma = sumarNum(10, 2);
+const suma2 = sumarElementos(1);
+
+console.log(suma);
+console.log(suma2);
