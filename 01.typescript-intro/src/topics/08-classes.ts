fd046c4aed9e5817f93d99ae8d4b8ed0cefd93d8
DanielRodriguezZara <danielrodrizarate@gmail.com>
2025-03-23 15:40:32 -0600

    Priorizar composición sobre herencia

--- 01.typescript-intro/src/topics/08-classes.ts
+++ 01.typescript-intro/src/topics/08-classes.ts
@@ -5,7 +5,11 @@ export class Person {
   //   private curp: string;
 
   // Así tambien podemos declarar las propiedades de una clase, añadirlas y asignarlas al contructor
-  constructor(public name: string, private curp: string) {
+  constructor(
+    public name: string,
+    public apellido: string,
+    private curp: string
+  ) {
     // Forma de asignarle los parametros del constructor a las propiedades de la clase
     // this.name = name
     // this.curp = curp
@@ -13,17 +17,23 @@ export class Person {
 }
 
 // Hacemos un extends de la clase Person para obtener información del constructor
-export class Escuela extends Person {
+export class Escuela {
   constructor(
     public turno: string,
     public grado: string,
-    public grupo: string
+    public grupo: string,
+    // Para hacer herencia es mejor instanciarla como una propiedad de la escuela
+    public estudiante: Person
   ) {
     //Con la palabra super hacemos referencia al constructor a la que se extiende
-    super("Danilos", "ROZD000502HTLDRNA9");
+    //super("Danilos", "ROZD000502HTLDRNA9");
   }
 }
 
-const danilos = new Escuela("Matutino", "Primero", "A");
+// Instanciamos una clase de persona
+const estudiante = new Person("Daniel", "Rodriguez", "ROZD000502HTLDRNA9");
+
+// Se la añadirmos como parametros en la instancia de la clase Escuela
+const danilos = new Escuela("Matutino", "Primero", "A", estudiante);
 
 console.log(danilos);
